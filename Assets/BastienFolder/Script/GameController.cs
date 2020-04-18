d906a9bb4bfd0ad672ccdf1c7a0817afd8241485
Bastien Lhuaire <bastien.lhuaire@outlook.fr>
2020-04-18 19:49:35 +0200

    UiController end Game Phase

---
+++ Assets/BastienFolder/Script/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public UIController UIController;
+    public int CurentLevel = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnPlay() {
+        UIController.OnPlayPress();
+    }
+
+    public void OnNextLevel() {
+        UIController.OnNextLevelPress();
+    }
+
+    public void EndGame() {
+        UIController.TriggerEndGame();
+    }
+
+    public void GameStart() {
+        UIController.TriggerGameStart();
+    }
+}
---
+++ Assets/BastienFolder/Script/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    private Animator UIAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIAnimator = GetComponent<Animator>();
+    }
+
+    public void OnPlayPress() {
+        UIAnimator.SetTrigger("Play");
+    }
+
+    public void OnNextLevelPress() {
+        UIAnimator.SetTrigger("NextLevel");
+    }
+
+    public void TriggerEndGame() {
+        UIAnimator.SetTrigger("EndGame");
+    }
+
+    public void TriggerGameStart() {
+        UIAnimator.SetTrigger("GameStart");
+    }
+}
