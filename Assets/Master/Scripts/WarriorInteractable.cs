13f35f70876f942b0a97fb4f4c39e344ea8bb741
Tom-GD/Dev <bardet.thomas@gmail.com>
2020-04-19 01:52:06 +0200

    Merge Saturday, multiple updates

---
+++ Assets/Master/Scripts/WarriorInteractable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_WarriorInterests
+{
+    //By priority
+    None = 0,
+    Sandwitch = 1,
+    Enemy = 9,
+    Chest = 10,
+    EmptyChest = 0,
+}
+
+public abstract class WarriorInteractable : MonoBehaviour
+{
+    public abstract void Interact();
+    public E_WarriorInterests interestType;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "WarriorSight")
+            Warrior.instance.See(this);
+    }
+}
