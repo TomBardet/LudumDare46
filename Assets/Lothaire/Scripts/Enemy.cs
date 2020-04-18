3764843f26a2c72db7e24ff16b28e893ad2189a5
lmeyre <lmeyre@student.42.fr>
2020-04-18 15:01:55 +0200

    Enemy et interactions

---
+++ Assets/Lothaire/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : WarriorInteractable
+{
+    public int hp;
+    public float aggroRange;
+
+    bool aggro = false;
+    Animator animator;
+    
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public override void Interact()
+    {
+        
+    }
+
+    // void Update()
+    // {
+    //     if (!aggro &&  Vector2.Distance(transform.position, Warrior.instance.transform.position) < aggroRange)
+    //     {
+    //         aggro = true;
+    //         S
+    //     }
+    // }
+
+    public void TakeDamage()
+    {
+        hp -= 1;
+        if (hp <= 0)
+            Death();
+        else
+            Attack();
+    }
+
+    void Attack()
+    {
+        Warrior.instance.TakeDamage(1);
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Lothaire/Scripts/WarriorInteractable.cs
+++ Assets/Lothaire/Scripts/WarriorInteractable.cs
@@ -2,7 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum E_WarriorInterests
+{
+    None = 0,
+    Enemy = 1,
+    Chest = 2,
+}
+
 public abstract class WarriorInteractable : MonoBehaviour
 {
     public abstract void Interact();
+    public E_WarriorInterests interestType;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "WarriorSight")
+            Warrior.instance.See(this);
+    }
 }
