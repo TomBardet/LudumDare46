5209ee1009293d2bba8f168c3df83fca61c14087
lmeyre <lmeyre@student.42.fr>
2020-04-18 13:06:50 +0200

    Base of warrior

---
+++ Assets/Lothaire/Scripts/Chest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : WarriorInteractable
+{
+    public override void Interact()
+    {
+        Debug.Log("Opening loot chest !");
+    }
+}
