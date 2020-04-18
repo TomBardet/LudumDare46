a4714328a6c65e67a45d5f0eb20f3b2b0ba8e7e9
Tom-GD/Dev <bardet.thomas@gmail.com>
2020-04-18 18:00:03 +0200

    Add graph in master, add turret, better grab, add plank, add bullet

--- Assets/ProtoTom/Scripts/GrabableItem.cs
+++ Assets/ProtoTom/Scripts/GrabableItem.cs
@@ -9,6 +9,8 @@ public class GrabableItem : Interactable
     public float WeightCarry;
     public float GrabDistance;
 
+    public Transform[] grabpoints;
+
     public Rigidbody2D rb;
     public bool isCarried;
 
@@ -33,5 +35,31 @@ public class GrabableItem : Interactable
     {
         if (isCarried) ReleaseBox();
         else CarryBox();
+    }
+
+    public Vector3 FindClosestPoint(Vector3 pos)
+    {
+        var distance = 0f;
+        var memoryDistance = 0f;
+        Transform pt = null;
+
+       foreach(Transform go in grabpoints)
+        {
+            distance = Vector3.Distance(go.position, pos);
+
+            if(distance < memoryDistance || memoryDistance == 0f)
+            {
+                memoryDistance = distance;
+                pt = go;
+            }
+        }
+
+        if (pt == null)
+        {
+            Debug.Log("No points found");
+            return new Vector3(0,0,0);
+        }
+        Debug.Log( pt.position);
+        return  pt.localPosition;
     }
 }
