1dd6c71a81fffb235f7b2b40c7763178b5f367bd
lmeyre <lmeyre@student.42.fr>
2020-04-18 11:47:47 +0200

    Update vscode

---
+++ Assets/Lothaire/Scripts/Warrior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : MonoBehaviour
+{
+    public static Warrior instance;
+
+    public float speed;
+    public int maxHp;
+
+    float hp;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+        
+        hp = maxHp;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WalkToTarget(Transform target)
+    {
+        while (Vector2.Distance(transform.position, target.position) < 0.1f)
+        yield return null;
+    }
+}
