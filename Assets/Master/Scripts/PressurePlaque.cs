aa326d030301a0c073020b46fa7029b2aa94eaf3
Tom-GD/Dev <bardet.thomas@gmail.com>
2020-04-19 18:54:41 +0200

    Pressure plate fix with moveable obj, integration assets

--- Assets/Master/Scripts/PressurePlaque.cs
+++ Assets/Master/Scripts/PressurePlaque.cs
@@ -10,22 +10,37 @@ public class PressurePlaque : MonoBehaviour
     public PlayableDirector TimelinePowerOn; 
     public PlayableDirector TimelinePowerOff;
 
+    public GameObject On;
+    public GameObject Off;
+
+    private void Start()
+    {
+        On.SetActive(false);
+        Off.SetActive(true);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Warrior"))
+        var c = collision.GetComponent<GrabableItem>();
+        if (collision.CompareTag("Warrior") ||c!= null && !c.isplank)
         {
             Power = true;
+            On.SetActive(true);
+            Off.SetActive(false);
 
             if (TimelinePowerOn == null) Debug.Log("Pas de timeline power on !");
 
             TimelinePowerOn.Play();
         }
+        
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Warrior"))
+        var c = collision.GetComponent<GrabableItem>();
+        if (collision.CompareTag("Warrior") || c != null && !c.isplank)
         {
             Power = false;
+            On.SetActive(false);
+            Off.SetActive(true);
 
             if (TimelinePowerOff == null) Debug.Log("Pas de timeline power off !");
 
