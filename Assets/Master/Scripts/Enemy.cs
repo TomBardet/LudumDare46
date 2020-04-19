83cd15d9ceb0bcd043a364968ef63038e822e41f
lmeyre <lmeyre@student.42.fr>
2020-04-19 15:25:04 +0200

    Healer mana + heal. Warrior upgrade and combat bugs fix, + mob upgrade. Encore qq soucis de comportements dans une sale

--- Assets/Master/Scripts/Enemy.cs
+++ Assets/Master/Scripts/Enemy.cs
@@ -6,8 +6,11 @@ public class Enemy : WarriorInteractable
 {
     public int hp;
     public float aggroRange;
-    public Enemy[] pack;
+    public List<Enemy> pack;
     public float chargeSpeed;
+    public float timeBetweenEachAttack;
+    public GameObject heartPrefab;
+    public Transform layout;
     bool aggro = false;
     Animator animator;
     Rigidbody2D body;
@@ -17,6 +20,12 @@ public class Enemy : WarriorInteractable
         animator = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
         interestType = E_WarriorInterests.Enemy;
+        for(int i = 0; i < hp; i++)
+            Instantiate(heartPrefab, layout);
+        if (!pack.Contains(this))
+            pack.Add(this);
+        if (chargeSpeed < Warrior.instance.chargeSpeed)
+            Debug.Log("Attention, ennemi plus lent que le warrior, il ne pourra pas le chase si le warrior va vers un coffre");
     }
 
     void OnDrawGizmosSelected()
@@ -32,51 +41,59 @@ public class Enemy : WarriorInteractable
     }
 
     void Update() 
-    { 
+    {
         if (!aggro && Vector2.Distance(transform.position, Warrior.instance.transform.position) < aggroRange) 
         { 
             foreach(Enemy enemy in pack) 
-                enemy.Aggro(); 
-        } 
-    } 
+                enemy.Aggro();
+        }
+    }
  
     public void Aggro() 
-    { 
-        aggro = true; 
+    {
+        Debug.Log("Enemy is aggro!");
+        aggro = true;
+        StopAllCoroutines();
         StartCoroutine(WalkToTarget(Warrior.instance.transform)); 
-    } 
-
+    }
 
     IEnumerator WalkToTarget(Transform warrior)
     {
-        while (Vector2.Distance(transform.position, warrior.transform.position) > 1f)
+        while (Vector2.Distance(transform.position, warrior.transform.position) > 2f)
         {
             Vector2 dir = (warrior.transform.position - transform.position).normalized;
             body.MovePosition((Vector2)transform.position + dir * chargeSpeed * Time.deltaTime);// Warrior run at double speed when see something, so ennemy at triple so it can catch up
             yield return null;
         }
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.1f);// So that warrior attack first
         Attack();
     }
 
     public void TakeDamage(int dmg)
     {
         hp -= dmg;
+        for(int i = 0; i < dmg; i++)
+            if(layout.transform.childCount > 0)
+                Destroy(layout.GetChild(0).gameObject);
         if (hp <= 0)
+        {
+            StopAllCoroutines();
             Invoke("Death", 1f);
+        }
         else
             Attack();
     }
 
     void Attack()
     {
-        Warrior.instance.TakeDamage(1, this);
+        Debug.Log("Goblin attack");
+        Warrior.instance.TakeDamage(10, this);
         StartCoroutine(AttackDelay());
     }
 
     IEnumerator AttackDelay()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(timeBetweenEachAttack);
         StartCoroutine(WalkToTarget(Warrior.instance.transform)); // If Warrior is going for loot and ignore mob, they have to walk again to catch him
     }
 
