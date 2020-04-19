d1dbd23499bdd42b17f52bf032fcb067f659e837
Tom-GD/Dev <bardet.thomas@gmail.com>
2020-04-20 01:37:02 +0200

    Commit intermediaire

---
+++ Assets/Master/Scripts/DoorEntrance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+public class DoorEntrance : MonoBehaviour
+{
+
+    PlayableDirector TimelineEntrance;
+    public Transform SpawnWarriorPos;
+    public Transform SpawnHealerPos;
+    public Transform parent;
+    public GameObject prefabWarrior;
+    public GameObject prefabHealer;
+    void Start()
+    {
+    }
+
+    //Utilisé dans des timelines
+    public void SpawnWarrior()
+    {
+        GameObject.Instantiate(prefabWarrior, parent);
+    }
+    
+    public void SpawnHealer()
+    {
+        GameObject.Instantiate(prefabHealer, parent);
+    }
+}
